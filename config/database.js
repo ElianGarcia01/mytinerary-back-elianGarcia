0e016966f9727a60338c1c7208f007ff1ce341da
Elian Garcia <telljua90@gmail.com>
2025-03-20 23:14:27 -0600

    Conexión a la base de datos

--- config/database.js
+++ config/database.js
@@ -2,7 +2,19 @@ import mongoose from "mongoose";
 
 let url = process.env.URI_MONGO;
 
-mongoose
-  .connect(url)
-  .then(() => console.log("database conected"))
-  .catch((error) => console.log(error))
+// mongoose
+//   .connect(url)
+//   .then(() => console.log("database conected"))
+//   .catch((error) => console.log(error))
+
+
+async function ConnectDB() {
+  try {
+    await mongoose.connect(url)
+    console.log("database connected")
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+ConnectDB()
